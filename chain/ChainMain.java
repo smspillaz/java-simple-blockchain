d23735e0bb3fe61a585c7fc748d91b68dbf9df13
Sam Spilsbury <smspillaz@gmail.com>
2017-04-28 12:27:11 +0800

    chain: Add simple HTTP server to post transactions to ChainMain

--- chain/ChainMain.java
+++ chain/ChainMain.java
@@ -1,6 +1,28 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
 public class ChainMain {
-    public static void main(String[] args) {
-        System.out.println("Hello, server world");
+    public static void main(String[] args) throws IOException {
+        System.out.println("ChainMain server running, post requests to /transaction");
+        HttpServer server = HttpServer.create(new InetSocketAddress(3002), 0);
+        server.createContext("/transaction", new ChainHTTPHandler());
+        server.start();
+    }
+
+    public static class ChainHTTPHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "Transaction Response";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream stream = exchange.getResponseBody();
+            stream.write(response.getBytes());
+            stream.close();
+        }
     }
 }
 
