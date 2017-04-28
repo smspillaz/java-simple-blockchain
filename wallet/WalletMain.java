df22c48f8651f28ece2e0e3946a96cec039a6b03
Sam Spilsbury <smspillaz@gmail.com>
2017-04-28 13:02:54 +0800

    wallet: Read host from commandline

--- wallet/WalletMain.java
+++ wallet/WalletMain.java
@@ -6,11 +6,19 @@ import java.util.Scanner;
 
 
 public class WalletMain {
+    public static void usage() {
+        System.err.println("WalletMain SERVER_HOST");
+    }
+
     public static void main(String[] args) throws IOException {
+        if (args.length < 1) {
+            usage();
+            System.exit(1);
+        }
+        String serverHost = args[0];
 
-        // Send a HTTP request
-
-        String url = "http://localhost:3002/transaction";
+        // Send a HTTP request to server
+        String url = "http://" + serverHost + ":3002/transaction";
 
         URLConnection connection = new URL(url).openConnection();
         connection.setRequestProperty("Accept-Charset", "UTF-8");
