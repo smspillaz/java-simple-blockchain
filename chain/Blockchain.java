71fa82c467a7b108c85713f8455b85cf2422c6d7
Sam Spilsbury <smspillaz@gmail.com>
2017-05-02 12:08:07 +0800

    blockchain: Add a genesis node to the chain on construction

--- chain/Blockchain.java
+++ chain/Blockchain.java
@@ -1,7 +1,19 @@
 import java.util.List;
+import java.util.ArrayList;
+import java.security.NoSuchAlgorithmException;
 
 public class Blockchain {
-    public Blockchain() {
-        /* On the construction of the blockchain, create a genesis node */
+    private List<Block> chain;
+    public Blockchain() throws NoSuchAlgorithmException {
+        chain = new ArrayList<Block>();
+        /* On the construction of the blockchain, create a genesis node.
+         * Note that right now, we are not signing transactions */
+        chain.add(new Block(new Transaction(0, 0, 50, 0),
+                            null));
+    }
+
+    public byte[] tipHash() {
+        Block lastBlock = chain.get(chain.size() - 1);
+        return lastBlock.hash;
     }
 }
