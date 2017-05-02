9e2ed92558b41ffe3641bf633ef327a299f53158
jpdsapp <19523162@student.uwa.edu.au>
2017-05-02 13:47:43 +0800

    2

--- chain/Miner.java
+++ chain/Miner.java
@@ -3,13 +3,15 @@
  */
 
 import javax.json.*;
+import java.io.StringReader;
 
 public class Miner {
     private byte[] senderPublicKey, receiverPublicKey, signature;
     private long amount;
     public boolean parseRequest(String json){
-        JSONObject obj = new JSONObject(json);
-        String pageName = .getJSONObject("pageInfo").getString("pageName");
+        javax.json.JsonObject jObj  = new JSONObject(new StringReader(json)); // json
+        String senderPublicKeyB64 = jObj.getString('spk');
+        String receiverPublicKeyB64 = jObj.getString('rpk');
 
     }
     public boolean walletRequest(byte[] senderPublicKey, byte[] receiverPublicKey, byte[] amount, byte[] signature);
