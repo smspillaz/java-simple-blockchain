bbd94244c1c79a9ab15c3be4f743cfffb5c38f9c
Sam Spilsbury <smspillaz@gmail.com>
2017-05-13 11:28:52 +0800

    chain: Add methods to walk the chain
    Such that transactions can be validated

--- chain/Blockchain.java
+++ chain/Blockchain.java
@@ -2,6 +2,16 @@ import java.util.List;
 import java.util.ArrayList;
 import java.security.NoSuchAlgorithmException;
 
+/**
+ * The Blockchain class just keeps a list of blocks and transactions.
+ * It doesn't care about whether the transactions themselves are valid, it
+ * is assumed that some wrapper class will inspect the chain in order to
+ * make sure that a transaction that is about to be appended makes sense.
+ *
+ * Each block comprises of a transaction and a parent block, and its hash
+ * is influced by the parent-most block's hash. The chain also provides a
+ * mechanism to iterate over all the prior transactions starting from the
+ * genesis to the child-most block in the chain */
 public class Blockchain {
     private List<Block> chain;
     public Blockchain() throws NoSuchAlgorithmException {
@@ -12,10 +22,39 @@ public class Blockchain {
                             null));
     }
 
+    public static class WalkFailedException extends Exception {
+        public WalkFailedException(String msg) {
+            /* Perhaps specify block hash */
+            super("Chain walk failed: " + msg);
+        }
+    }
+
+    public interface TransactionEnumerator {
+        void consume(int index, Transaction transaction) throws WalkFailedException;
+    }
+
+    public void walk(TransactionEnumerator enumerator) throws WalkFailedException {
+        int size = chain.size();
+        for (int i = 0; i < size; ++i) {
+            enumerator.consume(i, chain.get(i).getTransaction());
+        }
+    }
+
+    /**
+     * Appends a new transaction to the chain by creating a new block for it
+     * with a reference to the child-most block as its parent. Note that
+     * this does absolutely no validation to check if the transaction was
+     * valid - you will need to validate this before you append the block
+     * to the chain.
+     */
     public void appendTransaction(Transaction transaction) throws NoSuchAlgorithmException {
         chain.add(new Block(transaction, chain.get(chain.size() - 1)));
     }
 
+    /**
+     * This is just a convenience method to validate that a chain's child most
+     * block is what you expect it to be
+     */
     public byte[] tipHash() {
         Block lastBlock = chain.get(chain.size() - 1);
         return lastBlock.hash;
