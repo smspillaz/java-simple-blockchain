c1280effe2d19b61f5b6ecad1f99e507cca81947
Sam Spilsbury <smspillaz@gmail.com>
2017-05-13 12:24:48 +0800

    test: Add some new tests to ensure that transaction sanity checking works

--- test/LedgerTest.java
+++ test/LedgerTest.java
@@ -11,6 +11,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertThat;
 
 public class LedgerTest {
@@ -21,5 +22,114 @@ public class LedgerTest {
                                               Blockchain.WalkFailedException {
     Blockchain chain = new Blockchain();
     new Ledger(chain);
+  }
+
+  @Test(expected=Blockchain.WalkFailedException.class)
+  public void testLedgerFailedValidation() throws NoSuchAlgorithmException,
+                                                  Blockchain.WalkFailedException {
+    Blockchain chain = new Blockchain();
+
+    /* Try to blindly append an invalid transaction to the chain, i.e, public
+     * key (1) spending money that it doesn't have */
+    chain.appendTransaction(new Transaction(1, 0, 20, 0));
+
+    /* Now we create a new ledger from this chain.
+     * It should throw an exception, because the chain transactions are
+     * not valid */
+    new Ledger(chain);
+  }
+
+  @Test(expected=Blockchain.WalkFailedException.class)
+  public void testLedgerFailedValidationNegativeTransaction() throws NoSuchAlgorithmException,
+                                                                     Blockchain.WalkFailedException {
+    Blockchain chain = new Blockchain();
+
+    /* Try to blindly append a negative transaction. This wouldn't be allowed */
+    chain.appendTransaction(new Transaction(0, 1, -20, 0));
+
+    /* Now we create a new ledger from this chain.
+     * It should throw an exception, because the chain transactions are
+     * not valid */
+    new Ledger(chain);
+  }
+
+  @Test
+  public void testLedgerAddBadTransaction() throws NoSuchAlgorithmException,
+                                                   Blockchain.WalkFailedException {
+    Blockchain chain = new Blockchain();
+    Ledger ledger = new Ledger(chain);
+
+    /* Try to append an invalid transaction to the chain, i.e, public
+     * key (1) spending money that it doesn't have. The ledger should
+     * just silently reject it (it will return false so that we could
+     * potentially log that the transaction failed, but it won't throw
+     * an error) */
+    assertThat(ledger.appendTransaction(new Transaction(1, 0, 20, 0)),
+               equalTo(false));
+  }
+
+  @Test
+  public void testLedgerAddBadTransactionNoModifyChain() throws NoSuchAlgorithmException,
+                                                         Blockchain.WalkFailedException {
+    Blockchain chain = new Blockchain();
+    Ledger ledger = new Ledger(chain);
+
+    /* Try to append an invalid transaction to the chain, i.e, public
+     * key (1) spending money that it doesn't have. The ledger should
+     * just silently reject it and the chain should not be modified, eg
+     * the tip hash of both cases should be the same */
+    byte[] tip = chain.tipHash();
+    ledger.appendTransaction(new Transaction(1, 0, 20, 0));
+    assertThat(chain.tipHash(), equalTo(tip));
+  }
+
+  @Test
+  public void testLedgerAddNegativeTransaction() throws NoSuchAlgorithmException,
+                                                        Blockchain.WalkFailedException {
+    Blockchain chain = new Blockchain();
+    Ledger ledger = new Ledger(chain);
+
+    /* Whatever money people have, reverse transactions are not allowed */
+    assertThat(ledger.appendTransaction(new Transaction(0, 1, -20, 0)),
+               equalTo(false));
+  }
+
+  @Test
+  public void testLedgerAddGoodTransaction() throws NoSuchAlgorithmException,
+                                                    Blockchain.WalkFailedException {
+    Blockchain chain = new Blockchain();
+    Ledger ledger = new Ledger(chain);
+
+    /* Append a valid transation to the chain. It should return true
+     * since this transaction was all fine */
+    assertThat(ledger.appendTransaction(new Transaction(0, 1, 20, 0)),
+               equalTo(true));
+  }
+
+  @Test
+  public void testLedgerAddGoodTransactionModifyChain() throws NoSuchAlgorithmException,
+                                                               Blockchain.WalkFailedException {
+    Blockchain chain = new Blockchain();
+    Ledger ledger = new Ledger(chain);
+
+    /* Append a valid transation to the chain. The tip hash should be
+     * changed on the chain */
+    byte[] tip = chain.tipHash();
+    ledger.appendTransaction(new Transaction(0, 1, 20, 0));
+    assertThat(chain.tipHash(), not(tip));
+  }
+
+  @Test
+  public void testLedgerAddGoodTransactionReverseModifyChain() throws NoSuchAlgorithmException,
+                                                                     Blockchain.WalkFailedException {
+    Blockchain chain = new Blockchain();
+    Ledger ledger = new Ledger(chain);
+
+    /* Append a valid transation to the chain, then reverse it. The hashes
+     * should not be restored to their prior position */
+    byte[] tip = chain.tipHash();
+    ledger.appendTransaction(new Transaction(0, 1, 20, 0));
+    ledger.appendTransaction(new Transaction(1, 0, 20, 0));
+    assertThat(chain.tipHash(), not(tip));
   }
 }
