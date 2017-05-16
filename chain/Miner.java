aa6f6de3b9ddaaa3e4d19a3edc0c8ff6327303ac
Sam Spilsbury <smspillaz@gmail.com>
2017-05-16 22:06:02 +0800

    chain: Fix compilation errors in Miner

--- chain/Miner.java
+++ chain/Miner.java
@@ -2,17 +2,21 @@
  * Created by 19523162 on 2017-05-02.
  */
 // test @14/05/17 1745
-import javax.json.*;
+import javax.json.JsonObject;
 import java.io.StringReader;
 
 public class Miner {
     private byte[] senderPublicKey, receiverPublicKey, signature;
     private long amount;
-    public boolean parseRequest(String json){
-        javax.json.JsonObject jObj  = new JSONObject(new StringReader(json)); // json
-        String senderPublicKeyB64 = jObj.getString('spk');
-        String receiverPublicKeyB64 = jObj.getString('rpk');
 
+    public boolean parseRequest(String json){
+        JsonObject jObj = new JsonObject(new StringReader(json)); // json
+        String senderPublicKeyB64 = jObj.getString("spk");
+        String receiverPublicKeyB64 = jObj.getString("rpk");
+        return false;
     }
-    public boolean walletRequest(byte[] senderPublicKey, byte[] receiverPublicKey, byte[] amount, byte[] signature);
+
+    public boolean walletRequest(byte[] senderPublicKey, byte[] receiverPublicKey, byte[] amount, byte[] signature) {
+        return false;
+    }
 }
