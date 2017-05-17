f73cae3ac5b0dd031292a8379a976ffd9ab01cf0
Sam Spilsbury <smspillaz@gmail.com>
2017-05-17 10:11:54 +0800

    wallet: Use Args4j in the wallet too

--- wallet/WalletCLI.java
+++ wallet/WalletCLI.java
@@ -5,9 +5,44 @@ import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
 
+import javafx.application.Platform;
+
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
 public class WalletCLI {
-    public static void usage() {
-        System.err.println("WalletMain SERVER_HOST SERVER_JKS_KEYSTORE");
+    public static class Arguments {
+        @Option(name="-keystore", usage="The Client-Side Java KeyStore to use (mandatory)", metaVar="KEYSTORE")
+        public String keystore;
+
+        @Option(name="-host", usage="The blockchain host to connect to (mandatory)", metaVar="HOST")
+        public String host;
+
+        public Arguments(String args[]) {
+            CmdLineParser parser = new CmdLineParser(this);
+
+            try {
+                parser.parseArgument(args);
+
+                if (keystore == null) {
+                    throw new CmdLineException(parser, "Must provide a -keystore");
+                }
+
+                if (host == null) {
+                    throw new CmdLineException(parser, "Must provide a -host");
+                }
+
+                if (System.getenv("KEYSTORE_PASSWORD") == null) {
+                    throw new CmdLineException(parser, "Must set KEYSTORE_PASSWORD in the environment");
+                }
+            } catch (CmdLineException e) {
+                System.err.println(e.getMessage());
+                parser.printUsage(System.err);
+                Platform.exit();
+            }
+        }
     }
 
     public static void main(String[] args) throws IOException,
@@ -18,17 +53,12 @@ public class WalletCLI {
                                                   UnrecoverableKeyException,
                                                   Blockchain.WalkFailedException,
                                                   Blockchain.IntegrityCheckFailedException {
-        if (args.length < 2) {
-            usage();
-            System.exit(1);
-        }
-        String serverHost = args[0];
-        String serverCertificateKeyStore = args[1];
+        Arguments arguments = new Arguments(args);
 
         /* In the case of the commandline interface, if something fails, we
          * just throw an exception and let it propogate. */
-        WalletOrchestrator walletOrchestrator = new WalletOrchestrator(serverHost,
-                                                                       serverCertificateKeyStore,
+        WalletOrchestrator walletOrchestrator = new WalletOrchestrator(arguments.host,
+                                                                       arguments.keystore,
                                                                        System.getenv("KEYSTORE_PASSWORD"));
 
         System.out.println("Current balance: " + walletOrchestrator.ascertainBalance(0));
