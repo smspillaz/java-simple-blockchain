23e56d1a87526aaa984217efc27e3a423a5b9b11
Sam Spilsbury <smspillaz@gmail.com>
2017-05-25 20:44:00 +0800

    chain: Refactor away duplication in transaction handling

--- chain/AsynchronouslyMutableLedger.java
+++ chain/AsynchronouslyMutableLedger.java
@@ -28,9 +28,8 @@ public class AsynchronouslyMutableLedger extends Ledger {
         this.miner = miner;
     }
 
-    public static void logTransactionRejectionFailure(Transaction transaction,
-                                                      String reason) {
-        System.out.println("[chain] Rejecting transaction " + transaction + ": " + reason);
+    public static void logTransactionRejectionFailure(String reason) {
+        System.out.println("[chain] Rejecting transaction: " + reason);
     }
 
     /* Attempt to append a transaction to the underlying blockchain. If this
@@ -43,86 +42,21 @@ public class AsynchronouslyMutableLedger extends Ledger {
     public int appendSignedTransaction(SignedObject blob) throws NoSuchAlgorithmException {
         return miner.appendPayload(blob.serialize(), new BlockMiner.PayloadValidator() {
             public boolean validate(byte[] payload, int index) {
-                /* Re-create our SignedObject and Transaction from the mined
-                 * payload */
-                SignedObject blob = new SignedObject(payload);
-                Transaction transaction = new Transaction(blob.payload);
-
-                /* If the map doesn't contain an address, then add it with
-                 * a balance of zero chriscoins */
-                String srcMapKey = DatatypeConverter.printHexBinary(transaction.sPubKey);
-                String dstMapKey = DatatypeConverter.printHexBinary(transaction.rPubKey);
-                Ledger.maybeInitialiseMapEntry(ownership, srcMapKey, 0L);
-                Ledger.maybeInitialiseMapEntry(ownership, dstMapKey, 0L);
-
-                /* Transaction must have an amount */
-                if (transaction.amount <= 0) {
-                    logTransactionRejectionFailure(transaction,
-                                                   "amount is less than or equal to zero");
-                    return false;
-                }
-
-                /* We only want to do this check on non-genesis nodes, since
-                 * the genesis node will have to pay money to itself */
-                long srcCoins = ownership.get(srcMapKey);
-                if (index > 0 && srcCoins < transaction.amount) {
-                    logTransactionRejectionFailure(transaction,
-                                                   "source does not have requisite coins, " +
-                                                   "only has " + srcCoins);
-                    return false;
-                }
-
-                /* Self-transactions are not valid on genesis blocks */
-                if (index > 0 && srcMapKey.equals(dstMapKey)) {
-                    logTransactionRejectionFailure(transaction,
-                                                   " is a self transaction on a non-genesis block");
-                    return false;
-                }
-
-                /* Check to make sure that the signature is valid on the blob */
-                boolean signatureVerificationResult = false;
-
                 try {
-                    signatureVerificationResult = SignedObject.signatureIsValid(blob.payload,
-                                                                                blob.signature,
-                                                                                transaction.sPubKey);
-                } catch (InvalidKeyException e) {
-                    logTransactionRejectionFailure(transaction,
-                                                   "has an invalid public key: " + e.getMessage());
+                    validateAndProcessPayload(payload,
+                                              index,
+                                              ownership,
+                                              new ArrayList<TransactionObserver>());
+                } catch (Ledger.TransactionValidationFailedException e) {
+                    logTransactionRejectionFailure(e.getMessage());
                     return false;
-                } catch (InvalidKeySpecException e) {
-                    logTransactionRejectionFailure(transaction,
-                                                   "has an invalid public key PEM contents: " + e.getMessage());
+                } catch (Ledger.BlobSignatureValidationFailedException e) {
+                    logTransactionRejectionFailure(e.getMessage());
                     return false;
-                } catch (SignatureException e) {
-                    logTransactionRejectionFailure(transaction,
-                                                   "didn't have a processable signature: " + e.getMessage());
-                    return false;
-                } catch (NoSuchAlgorithmException e) {
-                    logTransactionRejectionFailure(transaction,
-                                                   " - requested hashing algorithm wasn't available: " + e.getMessage());
-                }
-
-                if (!signatureVerificationResult) {
-                    logTransactionRejectionFailure(transaction,
-                                                   "was not signed correctly, signature " +
-                                                   DatatypeConverter.printHexBinary(blob.signature) +
-                                                   " is invalid for public key " +
-                                                   DatatypeConverter.printHexBinary(transaction.sPubKey));
+                } catch (Blockchain.WalkFailedException e) {
+                    logTransactionRejectionFailure(e.getMessage());
                     return false;
                 }
-
-                /* Transaction would have been successful. Allow this transaction
-                 * on the chain and update our view.
-                 *
-                 * As above, the first transaction gives money to itself, so
-                 * just allow it to continue without updating the ledger */
-                if (index > 0) {
-                    ownership.put(srcMapKey,
-                                  srcCoins - transaction.amount);
-                }
-                ownership.put(dstMapKey,
-                              ownership.get(dstMapKey) + transaction.amount);
 
                 return true;
             }
@@ -132,9 +66,9 @@ public class AsynchronouslyMutableLedger extends Ledger {
                 SignedObject blob = new SignedObject(payload);
                 Transaction transaction = new Transaction(blob.payload);
 
-                logTransactionRejectionFailure(transaction,
-                                               "could not find a valid solution to " +
-                                               "mine a block, sorry");
+                logTransactionRejectionFailure("Couldn't find a valid solution to " +
+                                               "mine a block for " +
+                                               transaction + ", sorry");
             }
         });
     }
--- chain/Ledger.java
+++ chain/Ledger.java
@@ -68,6 +68,111 @@ public class Ledger {
         void consume(Transaction transaction);
     }
 
+    /* Check that a payload makes sense. This function returns normally
+     * if everything is fine, but throws exceptions in cases where
+     * a payload is malformed or nonsensical.
+     *
+     * If the payload made sense, update our internal view of
+     * the blockchain state. */
+    protected static void validateAndProcessPayload(byte[] payload,
+                                                    int index,
+                                                    Map<String, Long> ownership,
+                                                    List<TransactionObserver> observers) throws TransactionValidationFailedException,
+                                                                                                Blockchain.WalkFailedException,
+                                                                                                BlobSignatureValidationFailedException {
+        SignedObject blob = new SignedObject(payload);
+        Transaction transaction = new Transaction(blob.payload);
+
+        String srcMapKey = DatatypeConverter.printHexBinary(transaction.sPubKey);
+        String dstMapKey = DatatypeConverter.printHexBinary(transaction.rPubKey);
+
+        /* If the map doesn't contain an address, then add it with
+         * a balance of zero chriscoins.
+         *
+         * NOTE: Converting from byte arrays to digest strings like
+         * this is a pretty lazy way to get into a map, but perhaps
+         * later we can make the public keys a first class object */
+        Ledger.maybeInitialiseMapEntry(ownership,
+                                       srcMapKey,
+                                       0L);
+        Ledger.maybeInitialiseMapEntry(ownership,
+                                       dstMapKey,
+                                       0L);
+
+        long srcCoins = ownership.get(srcMapKey);
+
+        /* Reverse transactions are never allowed */
+        if (transaction.amount < 0) {
+            throw new TransactionValidationFailedException(transaction.sPubKey,
+                                                           srcCoins,
+                                                           transaction.rPubKey,
+                                                           transaction.amount);
+        }
+
+        /* On non-genesis blocks we need to perform transaction
+         * validation to ensure that nobody spends money that they
+         * don't have. On the genesis block however, we don't deduct
+         * money, only add it */
+        if (index > 0 &&
+            ownership.get(srcMapKey) < transaction.amount) {
+            throw new TransactionValidationFailedException(transaction.sPubKey,
+                                                           srcCoins,
+                                                           transaction.rPubKey,
+                                                           transaction.amount);
+        }
+
+        /* Self-transactions are not valid on genesis blocks */
+        if (index > 0 && srcMapKey.equals(dstMapKey)) {
+            throw new Blockchain.WalkFailedException(transaction +
+                                                     " is a self transaction on a non-genesis block");
+        }
+
+        boolean signatureVerificationResult = false;
+
+        try {
+            signatureVerificationResult = SignedObject.signatureIsValid(blob.payload,
+                                                                        blob.signature,
+                                                                        transaction.sPubKey);
+        } catch (NoSuchAlgorithmException e) {
+            /* Not much we can do here other than re-throw and abort
+             * the walk process */
+            throw new Blockchain.WalkFailedException(e.getMessage());
+        } catch (InvalidKeyException e) {
+            throw new Blockchain.WalkFailedException(transaction +
+                                                     " does not have a processable signature: " +
+                                                     e.getMessage());
+        } catch (InvalidKeySpecException e) {
+            throw new Blockchain.WalkFailedException(transaction +
+                                                     "has an invalid public key PEM contents: " +
+                                                     e.getMessage());
+        } catch (SignatureException e) {
+            throw new Blockchain.WalkFailedException(transaction +
+                                                     " has an invalid public key: " +
+                                                     e.getMessage());
+        }
+
+        /* Check to make sure that the signature is valid on the blob */
+        if (!signatureVerificationResult) {
+            throw new BlobSignatureValidationFailedException(transaction,
+                                                             blob.signature);
+        }
+
+        /* Transaction would have been successful. Allow this transaction
+         * on the chain and update our view */
+        if (index > 0) {
+            ownership.put(srcMapKey,
+                          Math.max(ownership.get(srcMapKey) -
+                                   transaction.amount, 0L));
+        }
+
+        ownership.put(dstMapKey,
+                      ownership.get(dstMapKey) + transaction.amount);
+
+        for (TransactionObserver observer : observers) {
+            observer.consume(transaction);
+        }
+    }
+
     /* Build up a view the transaction history for each public key and
      * address, optionally calling out to a TransactionObserver for each
      * transaction */
@@ -77,94 +182,10 @@ public class Ledger {
 
         chain.walk(new Blockchain.BlockEnumerator() {
             public void consume(int index, Block block) throws Blockchain.WalkFailedException {
-                SignedObject blob = new SignedObject(block.payload);
-                Transaction transaction = new Transaction(blob.payload);
-
-                String srcMapKey = DatatypeConverter.printHexBinary(transaction.sPubKey);
-                String dstMapKey = DatatypeConverter.printHexBinary(transaction.rPubKey);
-
-                /* If the map doesn't contain an address, then add it with
-                 * a balance of zero chriscoins.
-                 *
-                 * NOTE: Converting from byte arrays to digest strings like
-                 * this is a pretty lazy way to get into a map, but perhaps
-                 * later we can make the public keys a first class object */
-                Ledger.maybeInitialiseMapEntry(ownership,
-                                               srcMapKey,
-                                               0L);
-                Ledger.maybeInitialiseMapEntry(ownership,
-                                               dstMapKey,
-                                               0L);
-
-                long srcCoins = ownership.get(srcMapKey);
-
-                /* Reverse transactions are never allowed */
-                if (transaction.amount < 0) {
-                    throw new TransactionValidationFailedException(transaction.sPubKey,
-                                                                   srcCoins,
-                                                                   transaction.rPubKey,
-                                                                   transaction.amount);
-                }
-
-                /* On non-genesis blocks we need to perform transaction
-                 * validation to ensure that nobody spends money that they
-                 * don't have. On the genesis block however, we don't deduct
-                 * money, only add it */
-                if (index > 0 &&
-                    ownership.get(srcMapKey) < transaction.amount) {
-                    throw new TransactionValidationFailedException(transaction.sPubKey,
-                                                                   srcCoins,
-                                                                   transaction.rPubKey,
-                                                                   transaction.amount);
-                }
-
-                /* Self-transactions are not valid on genesis blocks */
-                if (index > 0 && srcMapKey.equals(dstMapKey)) {
-                    throw new Blockchain.WalkFailedException(transaction +
-                                                             " is a self transaction on a non-genesis block");
-                }
-
-                boolean signatureVerificationResult = false;
-
-                try {
-                    signatureVerificationResult = SignedObject.signatureIsValid(blob.payload,
-                                                                                blob.signature,
-                                                                                transaction.sPubKey);
-                } catch (NoSuchAlgorithmException e) {
-                    /* Not much we can do here other than re-throw and abort
-                     * the walk process */
-                    throw new Blockchain.WalkFailedException(e.getMessage());
-                } catch (InvalidKeyException e) {
-                    throw new Blockchain.WalkFailedException(transaction +
-                                                             " does not have a processable signature: " +
-                                                             e.getMessage());
-                } catch (InvalidKeySpecException e) {
-                    throw new Blockchain.WalkFailedException(transaction +
-                                                             "has an invalid public key PEM contents: " +
-                                                             e.getMessage());
-                } catch (SignatureException e) {
-                    throw new Blockchain.WalkFailedException(transaction +
-                                                             " has an invalid public key: " +
-                                                             e.getMessage());
-                }
-
-                /* Check to make sure that the signature is valid on the blob */
-                if (!signatureVerificationResult) {
-                    throw new BlobSignatureValidationFailedException(transaction,
-                                                                     blob.signature);
-                }
-
-                /* Transaction would have been successful. Allow this transaction
-                 * on the chain and update our view */
-                ownership.put(srcMapKey,
-                              Math.max(ownership.get(srcMapKey) -
-                                       transaction.amount, 0L));
-                ownership.put(dstMapKey,
-                              ownership.get(dstMapKey) + transaction.amount);
-
-                for (TransactionObserver observer : observers) {
-                    observer.consume(transaction);
-                }
+                validateAndProcessPayload(block.payload,
+                                          index,
+                                          ownership,
+                                          observers);
             }
         });
 
